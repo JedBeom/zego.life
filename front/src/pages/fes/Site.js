be149fcba6513573e858e9a261cf53fb5eb36ca6
Bombwhale <solkblte@icloud.com>
2021-01-31 16:45:44 +0900

    iframe loading becomes loader add U&I logo

--- front/src/pages/fes/Site.js
+++ front/src/pages/fes/Site.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const links = {
     "onair": "https://onair.zego.life",
-    "26hanbit": "calc(env(safe-area-inset-top) + 3em)"
+    "26hanbit": "https://26onlinehanbit.imweb.me/",
 }
 
 const Site = ({match}) => {
@@ -13,7 +13,7 @@ const Site = ({match}) => {
                 padding-top: env(safe-area-inset-top);
                 padding-left: 0;
                 padding-right: 0;
-                padding-bottom: 2rem;
+                max-width: unset;
             }
             .root-div {
                 overflow-y: hidden;
@@ -23,6 +23,7 @@ const Site = ({match}) => {
             }
             `}
         </style>
+        <div className="loader"/>
         <iframe frameBorder="0" className="fes-site" src={links[match.params.name]}/>
     </>
 }
