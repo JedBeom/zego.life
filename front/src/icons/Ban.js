507e8922fb37c910d8b792eb9a52d2e6a5089273
Bombwhale <solkblte@icloud.com>
2021-06-05 18:37:16 +0900

    ignore .csv files class -> className

--- front/src/icons/Ban.js
+++ front/src/icons/Ban.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Ban = () => <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler" viewBox="0 0 24 24">
+const Ban = () => <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler" viewBox="0 0 24 24">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <circle cx="12" cy="12" r="9"/>
     <line x1="5.7" y1="5.7" x2="18.3" y2="18.3"/>
