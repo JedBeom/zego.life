18a545c47592c4548424014496721654ab1e29ea
Bombwhale <solkblte@icloud.com>
2021-08-08 23:33:25 +0900

    Remove front

--- front/src/utils/hakbunToGCN.js
+++
@@ -1,9 +0,0 @@
-const hakbunToGCN = (hakbun) => {
-    let g = Number(hakbun.slice(0, 1))
-    let c = Number(hakbun.slice(1, 3))
-    let n = Number(hakbun.slice(3, 5))
-
-    return {g, c, n}
-}
-
-export default hakbunToGCN
--- front/src/utils/htmlDecode.js
+++
@@ -1,9 +0,0 @@
-function htmlDecode(input) {
-    var e = document.createElement('textarea');
-    e.innerHTML = input;
-    // handle case of empty input
-    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-}
-
-
-export default htmlDecode
